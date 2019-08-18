ecbabbab003830fcbd2d921ffc71c3fe4c101dc4
Rodrigo <rodrigogonsan1997@gmail.com>
2019-08-18 11:50:36 -0500

    archivo exe removido y cambios al codigo

--- fibo.cpp
+++ fibo.cpp
@@ -4,13 +4,14 @@
 int main(){
 	int num1=0,num2=1,x,num3,i;
 	printf("Introduce el numero de la sucesion\n");
-	scanf("%i",&x);
+	scanf("%d",&x);
 	for(i=0;i=x;i++){
 		num3=num1+num2;
 		num1=num2;
 		num2=num3;
 	}
-	printf("%d",&num3);
+	printf("%d",num3);
+	
 	system("pause");
 	return 0;
 }
