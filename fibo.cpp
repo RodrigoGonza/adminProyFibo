17c15a520619834aca7cda725fb1456c8e1f27db
Eduardo-MMz <54245711+Eduardo-MMz@users.noreply.github.com>
2019-08-18 17:34:44 -0500

    FiboUltimate.c
    Es la versión final sin recursión.
    Pueden probarlo, los términos de la sucesión van desde el 0 (F0=0) hasta el n-ésimo (Fn).

--- fibo.cpp
+++ fibo.cpp
@@ -1,18 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
 	int num1=0,num2=1,x,num3=0,i;
-	printf("Introduce el numero de la sucesion\n");
-	scanf("%d",&x);
+	printf("Introduce el termino de la sucesion de Fibonacci que quieras saber:");
+	scanf("%i",&x);
 	
-	for(i=0;i=x;i++)
+	for(i=1;i<=x;i++)
         {
-		num3=num1+num2;
-		num1=num2;
-		num2=num3;
-	}
-	printf("El termino %d de la sucesion de Fibonacci es: %d",x,num1);
+		 num3=num1+num2;
+		 num1=num2;
+		 num2=num3;
+		}
+	printf("El termino %i de la sucesion de Fibonacci es: %i\n",x,num1);
 	
 	system("pause");
 	return 0;
