b402112eab7ad2e79be42ccdb2f64dc308857513
GustavoHerrera97 <54246151+GustavoHerrera97@users.noreply.github.com>
2019-08-18 13:01:28 -0500

    Update fibo.cpp
    Mejor presentación en llaves

--- fibo.cpp
+++ fibo.cpp
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
+int main()
+{
 	int num1=0,num2=1,x,num3,i;
 	printf("Introduce el numero de la sucesion\n");
 	scanf("%d",&x);
-	for(i=0;i=x;i++){
+	for(i=0;i=x;i++)
+        {
 		num3=num1+num2;
 		num1=num2;
 		num2=num3;
